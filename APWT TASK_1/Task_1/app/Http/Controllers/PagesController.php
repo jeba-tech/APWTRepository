ba828b2a12d1d445c66f47cf8a43ee169b527773
JEBA <fawjiajeba@gmail.com>
2022-06-03 20:38:38 +0600

    Our Teams Section

--- APWT TASK_1/Task_1/app/Http/Controllers/PagesController.php
+++ APWT TASK_1/Task_1/app/Http/Controllers/PagesController.php
@@ -15,14 +15,10 @@ class PagesController extends Controller
     }
 
     public function profile(){
-        $name = "Mr. x";
-        $id="11-11111-1";
-        $dob = "10-10-2010";
+        
         $names=array("Mr. X", "Mr. Y", "Mr. Z");
         return view('profile')
-        ->with('name', $name)
-        ->with('id', $id)
-        ->with('dob', $dob)
+        
         ->with('names', $names);
     }
 }
--- APWT TASK_1/Task_1/resources/views/profile.blade.php
+++ APWT TASK_1/Task_1/resources/views/profile.blade.php
@@ -1,11 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
-<h1>Welcome to your profile {{$name}}</h1>
-<p>ID: {{$id}}</p>
-<p>DOB: {{$dob}}</p>
+<h1>Our Team Members</h1>
 
-<h2>Friends</h2>
 @foreach($names as $n)
 <li>{{$n}}</li>
 @endforeach
