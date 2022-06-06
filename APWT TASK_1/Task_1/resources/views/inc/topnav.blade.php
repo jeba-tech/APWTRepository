680d2a0222ac297ef0d290de0445942d825c3d75
JEBA <fawjiajeba@gmail.com>
2022-06-07 02:47:32 +0600

    navbar udpdated

--- APWT TASK_1/Task_1/resources/views/inc/topnav.blade.php
+++ APWT TASK_1/Task_1/resources/views/inc/topnav.blade.php
@@ -1,5 +1,4 @@
 <a class="btn btn-outline-secondary m-2 " href="{{route('home')}}">Home</a>
-
 <a class="btn btn-outline-secondary m-2 " href="{{route('productList')}}">Product</a>
 <a class="btn btn-outline-secondary m-2 " href="{{route('team')}}">Our Teams</a>
 <a class="btn btn-outline-secondary m-2 " href="{{route('about')}}">About us</a>
